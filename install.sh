2c96423aa652198f4427b9fb4123560db38e8e02
Goodup302 <j.f0471430704@gmail.com>
2020-01-03 09:21:38 +0100

    Add docker image Ngnix

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@ apt-add-repository ppa:ansible/ansible
 apt-get update -y
 apt-get upgrade -y
 
-apt-get install -y nano vim apache2 ansible
+apt-get install -y nano vim ansible
 
 apt-get remove -y docker docker-engine docker.io
 apt install -y docker.io
 systemctl start docker
 systemctl enable docker
+
+git clone https://github.com/Goodup302/Docker-test.git
+cd Docker-test
+docker build . -t test
+docker run -d -p 80:80 test
